741bd4cb72976b896a8e17d8bc7fb4dbfa008a48
Lorenzo Balzani <lorenzo.balzani@studio.unibo.it>
2020-02-06 16:28:06 +0100

    Added initView method in GameLauncher and button to launch the game

--- src/main/java/it/cubicworldsimulator/game/GameLauncher.java
+++ src/main/java/it/cubicworldsimulator/game/GameLauncher.java
@@ -3,18 +3,37 @@ package it.cubicworldsimulator.game;
 import it.cubicworldsimulator.engine.GameEngine;
 import it.cubicworldsimulator.engine.GameLogic;
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+
 public class GameLauncher extends Application {
+
+    private Stage primaryStage;
+
     @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Cubic World Simulator");
-        Stage stage = new Stage();
-        stage.initModality(Modality.WINDOW_MODAL);
-        stage.initOwner(primaryStage);
-        primaryStage.show();
-        stage.showAndWait();
+    public void start(final Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.initView();
+        this.primaryStage.showAndWait();
+    }
+
+    private void initView() {
+        this.primaryStage.setTitle("Cubic World Simulator");
+        this.primaryStage.setWidth(1920);
+        this.primaryStage.setHeight(1080);
+        this.primaryStage.initModality(Modality.WINDOW_MODAL);
+        Button launchGameButton = new Button("Launch game");
+        launchGameButton.setStyle("-fx-font-size: 15pt;");
+        launchGameButton.setOnAction( actionEvent -> {
+            this.runGame();
+        });
+        launchGameButton.setAlignment(Pos.CENTER);
+        Scene scene = new Scene(launchGameButton, 200, 100);
+        this.primaryStage.setScene(scene);
     }
 
     private void runGame() {
