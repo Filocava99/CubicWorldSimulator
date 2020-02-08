f6a4c6b5248a7372212a1b584588f664d7e5e962
Filippo Cavallari <filippo.cavallari99@gmail.com>
2020-02-08 09:51:11 +0100

    Added the texture support in the Renderer class

--- src/main/java/it/cubicworldsimulator/engine/Renderer.java
+++ src/main/java/it/cubicworldsimulator/engine/Renderer.java
@@ -55,6 +55,7 @@ public class Renderer {
         // Update projection Matrix
         Matrix4f projectionMatrix = transformation.getProjectionMatrix(FOV, window.getWidth(), window.getHeight(), Z_NEAR, Z_FAR);
         shaderProgram.setUniform("projectionMatrix", projectionMatrix);
+        shaderProgram.setUniform("texture_sampler", 0);
 
         // Render each gameItem
         for (GameItem gameItem : gameItems) {
