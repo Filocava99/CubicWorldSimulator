e4736bec472d2f1ad6f74fbfc082b352dae11386
balzanilo99 <balzanilo@icloud.com>
2020-02-11 13:31:06 +0100

    Deleted GameLauncher class, 'cause LWJGL and others window toolkit (AWT, Swing, JavaFX...) are incompatible on Mac OS platform.

--- src/main/java/it/cubicworldsimulator/game/Main.java
+++ src/main/java/it/cubicworldsimulator/game/Main.java
@@ -1,10 +1,18 @@
 package it.cubicworldsimulator.game;
 
-
-import javafx.application.Application;
+import it.cubicworldsimulator.engine.GameEngine;
+import it.cubicworldsimulator.engine.GameLogic;
 
 public class Main {
-    public static void main(String[] args) {
-        GameLauncher.launch(GameLauncher.class, args);
+    public static void main(String[] args) throws Exception {
+        try {
+            GameLogic gameLogic = new Game();
+            GameEngine gameEngine = new GameEngine("CubicWorldSimulator", 600, 480,
+                    true, gameLogic);
+                    gameEngine.run();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
     }
 }
