5d7277e24f9b9d8d534ecdb1241b0c71f3040473
Filippo Cavallari <filippo.cavallari99@gmail.com>
2020-02-14 14:27:40 +0100

    Added getters

--- src/main/java/it/cubicworldsimulator/game/world/block/Material.java
+++ src/main/java/it/cubicworldsimulator/game/world/block/Material.java
@@ -2,13 +2,25 @@ package it.cubicworldsimulator.game.world.block;
 
 public class Material {
 
-    private final int id;
+    private final byte id;
     private final String name;
     private final BlockTexture blockTexture;
 
-    public Material(int id, String name, BlockTexture blockTexture) {
+    public Material(byte id, String name, BlockTexture blockTexture) {
         this.id = id;
         this.name = name;
         this.blockTexture = blockTexture;
+    }
+
+    public byte getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BlockTexture getBlockTexture() {
+        return blockTexture;
     }
 }
