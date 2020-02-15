3e669be44601adb1cde63de3f7fbb589dea996c5
balzanilo99 <balzanilo@icloud.com>
2020-02-15 12:28:09 +0100

    Refactored methods names in Loader interface

--- src/main/java/it/cubicworldsimulator/engine/Loader.java
+++ src/main/java/it/cubicworldsimulator/engine/Loader.java
@@ -3,7 +3,7 @@ package it.cubicworldsimulator.engine;
 public interface Loader {
     int createVao();
     int createVbo();
-    void insertFloatIntoVbo(int vboId, float[] data, int index, int target, int elementDimension);
-    void insertIntIntoVbo(int vboId, int[] data, int index, int target);
+    void insertDataIntoVbo(int vboId, float[] data, int index, int target, int elementDimension);
+    void insertIndices(int vboId, int[] data, int indexToBind, int target);
     void cleanUp();
 }
