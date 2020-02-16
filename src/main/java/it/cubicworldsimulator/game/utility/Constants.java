9b2fa05d93f0e34ea6fce5f718dd4c943e86583f
Filippo Cavallari <filippo.cavallari99@gmail.com>
2020-02-16 18:56:42 +0100

    Created class ChunkLoader

--- src/main/java/it/cubicworldsimulator/game/utility/Constants.java
+++ src/main/java/it/cubicworldsimulator/game/utility/Constants.java
@@ -1,5 +1,7 @@
 package it.cubicworldsimulator.game.utility;
 
+import java.io.File;
+
 public class Constants {
 
     public static final int chunkAxisSize = 16;
@@ -7,10 +9,19 @@ public class Constants {
     public static final int chunksPerColumn = 16;
     public static final int maxHeight;
     public static final int minHeight = 0;
+    public static final String installationFolder;
 
     static {
         chunkTotalBlocks = chunkAxisSize * chunkAxisSize * chunkAxisSize;
         maxHeight = chunkAxisSize * chunksPerColumn;
+        final String osName = System.getProperty("os.name");
+        if(osName.contains("Windows")){
+            installationFolder = System.getProperty("user.home") + File.pathSeparator + "Desktop/CubicWorldSimulator";
+        }else if(osName.contains("Mac")){
+            installationFolder = System.getProperty("user.home") + File.pathSeparator + "Desktop/CubicWorldSimulator";
+        }else{
+            installationFolder = System.getProperty("user.home") + File.pathSeparator + "Desktop/CubicWorldSimulator";
+        }
     }
 
 }
