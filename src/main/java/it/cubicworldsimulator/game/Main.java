f00ae74d075662aa0ee7ae16f8a4edb5254cc87e
Filippo Cavallari <filippo.cavallari99@gmail.com>
2020-02-16 15:00:29 +0100

    Fixed arrayOutOfBoundException

--- src/main/java/it/cubicworldsimulator/game/Main.java
+++ src/main/java/it/cubicworldsimulator/game/Main.java
@@ -6,8 +6,10 @@ import it.cubicworldsimulator.engine.GameLogic;
 public class Main {
     public static void main(String[] args) throws Exception {
         boolean debug = false;
-        if (args.length > 0 && args[0].equalsIgnoreCase("false") || args[0].equalsIgnoreCase("true")) {
-            debug = Boolean.parseBoolean(args[0]);
+        if (args != null && args.length > 0) {
+            if(args[0].equalsIgnoreCase("false") || args[0].equalsIgnoreCase("true")) {
+                debug = Boolean.parseBoolean(args[0]);
+            }
         }
         try {
             GameLogic gameLogic = new Game();
