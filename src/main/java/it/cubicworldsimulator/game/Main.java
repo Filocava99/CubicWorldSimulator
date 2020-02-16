224114cb7b4e7e4f336354315c188c81901f6982
lorenzobalzani <balzanilo@gmail.com>
2020-02-16 11:55:58 +0100

    Modified args.length control in main method

--- src/main/java/it/cubicworldsimulator/game/Main.java
+++ src/main/java/it/cubicworldsimulator/game/Main.java
@@ -6,7 +6,7 @@ import it.cubicworldsimulator.engine.GameLogic;
 public class Main {
     public static void main(String[] args) throws Exception {
         boolean debug = false;
-        if (args.length > 1 && args[0].equalsIgnoreCase("false") || args[0].equalsIgnoreCase("true")) {
+        if (args.length > 0 && args[0].equalsIgnoreCase("false") || args[0].equalsIgnoreCase("true")) {
             debug = Boolean.parseBoolean(args[0]);
         }
         try {
