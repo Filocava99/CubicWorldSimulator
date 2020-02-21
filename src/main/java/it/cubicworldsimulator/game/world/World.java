80d067496a79998b030f4877e2af5c4176e013ec
Filippo Cavallari <filippo.cavallari99@gmail.com>
2020-02-21 16:23:30 +0100

    Edited WorldManager constructor

--- src/main/java/it/cubicworldsimulator/game/world/World.java
+++ src/main/java/it/cubicworldsimulator/game/world/World.java
@@ -1,16 +1,17 @@
 package it.cubicworldsimulator.game.world;
 
 import it.cubicworldsimulator.game.world.chunk.Chunk;
+import it.cubicworldsimulator.game.world.chunk.ChunkColumn;
+import org.joml.Vector2f;
 
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class World {
 
     private final List<Chunk> chunks;
     private final long seed;
     private final String name; //Non si puo' cambiare il nome del mondo a runtime. Oppure vogliamo lasciarlo fare? Boh
+    private final Map<Vector2f, ChunkColumn> activeChunks = new HashMap<>();
 
     public World(String name, long seed){
         chunks = new LinkedList<>(); //TODO Valutare la scelta della linkedList
