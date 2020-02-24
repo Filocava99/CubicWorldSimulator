cd82ed1ff00d5e05f20d59456261e7246188f2e5
balzanilo99 <balzanilo@icloud.com>
2020-02-24 11:11:26 +0100

    Changed glfwDestroyWindow to glfwHideWindow

--- src/main/java/it/cubicworldsimulator/game/Main.java
+++ src/main/java/it/cubicworldsimulator/game/Main.java
@@ -1,6 +1,7 @@
 package it.cubicworldsimulator.game;
 
 import it.cubicworldsimulator.engine.GameEngine;
+import it.cubicworldsimulator.game.gui.Gui;
 import it.cubicworldsimulator.game.gui.LauncherGui;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -9,26 +10,17 @@ import org.joml.Vector2i;
 public class Main {
 
     private static final Logger logger = LogManager.getLogger(Main.class);
+    private static final GuiFactory guiFactory = new GuiFactory();
 
-    public static void main(String[] args) throws Exception {
-        boolean debug = false;
-        boolean vsync = true;
+    public static void main(String[] args) {
+        final int width = 800;
+        final int height = 800;
 
-        //TODO Aggiungere vSync come parametro di avvio
-        if (args != null && args.length > 0) {
-            if(args[0].equalsIgnoreCase("false") || args[0].equalsIgnoreCase("true")) {
-                debug = Boolean.parseBoolean(args[0]);
-            }
-        }
         try {
             logger.debug("Platform: " + System.getProperty("os.name"));
-            logger.info("VSync: " + vsync);
-            logger.debug("Debug mode: " + debug);
-            logger.trace("Game running...");
-            GameEngine gameEngine = new GameEngine("CubicWorldSimulator",
-                    true, new Game(), false);
-            final Vector2i size = new Vector2i(800, 750);
-            GuiFactory.createGui(new LauncherGui(size), size, "CubicWorldSimulator Launcher");
+            logger.trace("Game launcher started...");
+            final var size = new Vector2i(width, height);
+            guiFactory.createGui(new LauncherGui(size), size, "CubicWorldSimulator Launcher");
         } catch (Exception e) {
             logger.error(e.getMessage());
             e.printStackTrace();
