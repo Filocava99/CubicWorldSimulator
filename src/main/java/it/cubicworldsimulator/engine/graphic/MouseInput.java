8505e18337852220f3f1b6feae57e9c6df0daad0
davidedomini <davidedomini@gmail.com>
2020-02-24 15:27:56 +0100

    made constructor and init method

--- src/main/java/it/cubicworldsimulator/engine/graphic/MouseInput.java
+++ src/main/java/it/cubicworldsimulator/engine/graphic/MouseInput.java
@@ -1,5 +1,40 @@
 package it.cubicworldsimulator.engine.graphic;
 
-public class MouseInput {
+import org.joml.Vector2d;
 
+import it.cubicworldsimulator.engine.Window;
+import static org.lwjgl.glfw.GLFW.*;
+
+public class MouseInput {
+	private final Vector2d previousPosition;
+	private final Vector2d currentPosition;
+	private final Vector2d displacementVector;
+	private boolean pointerInWindow;
+	private boolean leftButtonPressed;
+	private boolean rightButtonPressed;
+	
+	public MouseInput() {
+		this.previousPosition = new Vector2d(-1, -1);
+		this.currentPosition = new Vector2d(0, 0);
+		this.displacementVector = new Vector2d();
+		this.pointerInWindow = false;
+		this.leftButtonPressed = false;
+		this.rightButtonPressed = false;
+	}
+	
+	public void init(Window window) {
+		glfwSetCursorPosCallback(window.getWindowHandle(), (windowHandle, xpos, ypos) ->{
+			this.currentPosition.x = xpos;
+			this.currentPosition.y = ypos;
+		});
+		glfwSetCursorEnterCallback(window.getWindowHandle(), (windowHandle, entered)->{
+			this.pointerInWindow = entered;
+		});
+		glfwSetMouseButtonCallback(window.getWindowHandle(), (windowHandle, button, action, mode)->{
+			this.leftButtonPressed = (button == GLFW_MOUSE_BUTTON_1 && action == GLFW_PRESS);
+			this.rightButtonPressed = (button == GLFW_MOUSE_BUTTON_2 && action == GLFW_PRESS);
+		});
+	}
+	
+	
 }
