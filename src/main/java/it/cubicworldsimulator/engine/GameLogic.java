927374d206780cb62ebd2e176bae5f64a74416e0
davidedomini <davidedomini@gmail.com>
2020-02-25 10:14:20 +0100

    updated GameEngine class to support mouse inputs

--- src/main/java/it/cubicworldsimulator/engine/GameLogic.java
+++ src/main/java/it/cubicworldsimulator/engine/GameLogic.java
@@ -1,11 +1,13 @@
 package it.cubicworldsimulator.engine;
 
+import it.cubicworldsimulator.engine.graphic.MouseInput;
+
 public interface GameLogic {
     void init(Window window) throws Exception;
 
-    void input(Window window);
+    void input(Window window, MouseInput mouseInput);
 
-    void update(float interval);
+    void update(float interval, MouseInput mouseInput);
 
     void render(Window window);
 
