840a28718675bffb2b077e83bd86677c1431760e
Filippo Cavallari <filippo.cavallari99@gmail.com>
2020-02-28 13:47:12 +0100

    Extended YAML APIs

--- src/main/java/it/cubicworldsimulator/game/utility/yaml/YAMLLoader.java
+++ src/main/java/it/cubicworldsimulator/game/utility/yaml/YAMLLoader.java
@@ -1,17 +1,13 @@
 package it.cubicworldsimulator.game.utility.yaml;
 
-import it.cubicworldsimulator.game.world.block.BlockTexture;
-import it.cubicworldsimulator.game.world.block.Material;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.joml.Vector2f;
 import org.snakeyaml.engine.v1.api.Load;
 import org.snakeyaml.engine.v1.api.LoadSettingsBuilder;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.util.Map;
 
 public class YAMLLoader {
 
@@ -19,7 +15,7 @@ public class YAMLLoader {
 
     private final Load loader;
 
-    public YAMLLoader() throws FileNotFoundException {
+    public YAMLLoader(){
         loader = new Load(new LoadSettingsBuilder().setLabel("loader").build());
     }
 
