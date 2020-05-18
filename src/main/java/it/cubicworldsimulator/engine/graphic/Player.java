1dbda9a639847781509e45948a6027987ad35415
davidedomini <davidedomini@gmail.com>
2020-05-18 19:27:11 +0200

    refactor player class, now no longer extends camera class

--- src/main/java/it/cubicworldsimulator/engine/graphic/Player.java
+++ src/main/java/it/cubicworldsimulator/engine/graphic/Player.java
@@ -5,33 +5,34 @@ import it.cubicworldsimulator.game.utility.Constants;
 import org.joml.Vector3f;
 import org.joml.Vector3i;
 
-public class Player extends Camera{
+public class Player{
 
     private Vector3f lastPos;
     private Vector3i lastChunk;
     private GameItem playerModel;
+    private Camera camera;
     
     public Player(String objModel, String textureFile){
+    	this.camera = new Camera();
     	OBJLoader objLoader = new OBJLoader();
 		try {
 			Mesh playerMesh = objLoader.loadFromOBJ(objModel, textureFile);
 			this.playerModel = new GameItem(playerMesh);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
     }
 
     public boolean didPlayerMove(){
-        final boolean result = getPosition().equals(lastPos);
+        final boolean result = this.camera.getPosition().equals(lastPos);
         if(result){
-            lastPos = getPosition();
+            lastPos = this.camera.getPosition();
         }
         return result;
     }
 
     public boolean didPlayerChangedChunk(){
-        Vector3i chunkCoord = worldCoordToChunkCoord(getPosition());
+        Vector3i chunkCoord = worldCoordToChunkCoord(this.camera.getPosition());
         final boolean result = !chunkCoord.equals(lastChunk);
         if(result){
             lastChunk = chunkCoord;
@@ -40,7 +41,7 @@ public class Player extends Camera{
     }
 
     private Vector3i worldCoordToChunkCoord(Vector3f position) {
-        return new Vector3i((int) Math.floor(position.x / Constants.chunkAxisSize), (int) Math.floor(position.y / Constants.chunkAxisSize), (int) Math.floor(position.z / Constants.chunkAxisSize));
+        return new Vector3i((int) Math.floor(this.camera.getPosition().x / Constants.chunkAxisSize), (int) Math.floor(this.camera.getPosition().y / Constants.chunkAxisSize), (int) Math.floor(this.camera.getPosition().z / Constants.chunkAxisSize));
     }
 
     public Vector3i getChunkPosition() {
@@ -49,5 +50,9 @@ public class Player extends Camera{
     
     public GameItem getPlayerModel() {
     	return this.playerModel;
+    }
+    
+    public Camera getCamera() {
+    	return this.camera;
     }
 }
