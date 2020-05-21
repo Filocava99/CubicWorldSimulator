0fe08d63b573230ee3bd3c40587986986753839a
balzanilo99 <balzanilo@icloud.com>
2020-05-21 13:57:26 +0200

    Created info window and refactored changeTheme method.

--- src/main/java/it/cubicworldsimulator/game/gui/GenericGui.java
+++ src/main/java/it/cubicworldsimulator/game/gui/GenericGui.java
@@ -5,6 +5,11 @@ import org.liquidengine.legui.component.Button;
 import org.liquidengine.legui.component.Label;
 import org.liquidengine.legui.component.Panel;
 import org.liquidengine.legui.component.TextInput;
+import org.liquidengine.legui.style.color.ColorConstants;
+import org.liquidengine.legui.theme.Themes;
+import org.liquidengine.legui.theme.colored.FlatColoredTheme;
+
+import static org.liquidengine.legui.style.color.ColorUtil.fromInt;
 
 public abstract class GenericGui extends Panel {
     protected final GuiFactory guiFactory;
@@ -17,6 +22,11 @@ public abstract class GenericGui extends Panel {
         guiFactory.setHeight((int) height);
         this.setAspectRatio();
     }
+
+    /**
+     * It creates gui components
+     */
+    protected abstract void createGui();
 
     public void setAspectRatio() {
         guiFactory.setAspectRatio();
@@ -46,5 +56,28 @@ public abstract class GenericGui extends Panel {
 
     protected void setWindowId(long windowId) {
         this.windowId = windowId;
+    }
+
+    /**
+     * Switch to the different theme
+     */
+    protected abstract void changeTheme();
+
+    protected void flatColoredTheme() {
+        Themes.setDefaultTheme(new FlatColoredTheme(
+                fromInt(44, 62, 80, 1), // backgroundColor
+                fromInt(127, 140, 141, 1), // borderColor
+                fromInt(127, 140, 141, 1), // sliderColor
+                fromInt(2, 119, 189, 1), // strokeColor
+                fromInt(39, 174, 96, 1), // allowColor
+                fromInt(192, 57, 43, 1), // denyColor
+                fromInt(0, 0, 0, 1f),  // shadowColor
+                ColorConstants.white()
+        ));
+       applyTheme();
+    }
+
+    private void applyTheme() {
+        Themes.getDefaultTheme().applyAll(this);
     }
 }
