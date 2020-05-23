d186ec366e5bc26bd798c74e0b452194d63e84e2
lorenzobalzani <balzanilo@gmail.com>
2020-05-23 14:10:53 +0200

    Added VBO class. It's a wrapper for int value vboId.

--- src/main/java/it/cubicworldsimulator/engine/graphic/Mesh.java
+++ src/main/java/it/cubicworldsimulator/engine/graphic/Mesh.java
@@ -1,18 +1,20 @@
 package it.cubicworldsimulator.engine.graphic;
 
+import it.cubicworldsimulator.engine.loader.VBO;
+
 import java.util.List;
 
 public class Mesh {
 
-    private int vaoId;
-    private final List<Integer> vboList;
-    private final List<Integer> textureVboList;
+    private final int vaoId;
+    private final List<VBO> vboList;
+    private final List<VBO> textureVboList;
     private final int vertexCount;
     private final Material material;
     private final float boundingRadius;
 
     public Mesh(Material texture, float boundingRadius, int vertexCount, int vaoId,
-                List<Integer> vboList, List<Integer> textureVboList) {
+                List<VBO> vboList, List<VBO> textureVboList) {
         this.material = texture;
         this.boundingRadius = boundingRadius;
         this.vertexCount = vertexCount;
@@ -37,11 +39,11 @@ public class Mesh {
         return material;
     }
 
-    public List<Integer> getVboList() {
+    public List<VBO> getVboList() {
         return vboList;
     }
 
-    public List<Integer> getTextureVboList() {
+    public List<VBO> getTextureVboList() {
         return textureVboList;
     }
 }
---
+++ src/main/java/it/cubicworldsimulator/engine/loader/VBO.java
@@ -0,0 +1,13 @@
+package it.cubicworldsimulator.engine.loader;
+
+public class VBO {
+    int Id;
+
+    public VBO(int Id) {
+        this.Id = Id;
+    }
+
+    public int getId() {
+        return Id;
+    }
+}
