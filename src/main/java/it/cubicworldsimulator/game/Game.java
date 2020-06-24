ce74c2456ffbde5c6fbaf95ee37b5b5831cb1520
davidedomini <davidedomini@gmail.com>
2020-06-24 14:55:54 +0200

    refactoring player

--- src/main/java/it/cubicworldsimulator/game/Game.java
+++ src/main/java/it/cubicworldsimulator/game/Game.java
@@ -9,7 +9,6 @@ import it.cubicworldsimulator.engine.graphic.light.DirectionalLight;
 import it.cubicworldsimulator.engine.graphic.light.LightCycleManager;
 import it.cubicworldsimulator.engine.graphic.light.LightFactory;
 import it.cubicworldsimulator.engine.graphic.light.LightFactoryImpl;
-import it.cubicworldsimulator.engine.graphic.light.PointLight;
 import it.cubicworldsimulator.engine.graphic.light.SceneLight;
 import it.cubicworldsimulator.engine.renderer.RendererImpl;
 import it.cubicworldsimulator.game.gui.Settings;
@@ -77,8 +76,7 @@ public class Game implements GameLogic {
             float specularPower = 10f;
             float lightIntensity = 1.0f;
             
-            PointLight pointLight = lightFactory.createPointLight(lightColour, lightPosition, lightIntensity);
-         
+           
             lightPosition = new Vector3f(-1, 0, 0);
             lightColour = new Vector3f(1, 1, 1);
             DirectionalLight directionalLight = lightFactory.createDirectionalLight(lightColour, lightPosition, lightIntensity);
